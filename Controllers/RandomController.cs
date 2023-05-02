b9d14141bd1c4b46633b0f558654dfe343a5fa1f
vadymps <vadym.pastushenko7@gmail.com>
2023-05-02 12:22:11 +0200

    create project

---
+++ Controllers/RandomController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace random_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RandomController : ControllerBase
+    {
+        private readonly ILogger<RandomController> _logger;
+
+        public RandomController(ILogger<RandomController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(new Random().Next().ToString()[..5]);
+        }
+    }
+}
